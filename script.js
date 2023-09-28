0618f3f390c068144a15794cc4943066dabc9b84
jiankh <131821436+jiankh@users.noreply.github.com>
2023-09-28 12:45:58 -0400

    added HTML and styled the page with CSS

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const email = document.getElementById("email");
+const country = document.getElementById("country");
+const zipcode = document.getElementById("zipcode");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
